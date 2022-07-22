d5f94c415ea3e0aa789ea22852495597384f2d57
Amit Verma <83941820+amitvneosoft@users.noreply.github.com>
2022-07-22 11:04:27 +0530

    Update main.go

--- 05pointers/main.go
+++ 05pointers/main.go
@@ -5,15 +5,27 @@ import (
 )
 
 func main() {
+	myPointer := 50
+	refPointer := &myPointer
 
-	var welcome string = "Welcome to a class of pointers"
-	fmt.Println(welcome)
+	fmt.Println("My pointer value is: ", myPointer)
+	fmt.Println("My pointer memory location is: ", refPointer)
+	fmt.Println("My ref pointer value is: ", *refPointer)
 
-	myPointer := 54
+	anotherPtr := myPointer
+	
+	fmt.Println("Another assigned var value is: ", anotherPtr)
 
-	var refPointer = &myPointer
+	myPointer = 55
 
-	fmt.Println("My pointer value is ", refPointer)
-	fmt.Println("My pointer value is ", *refPointer)
+	fmt.Println("My pointer value is: ", myPointer)
+	fmt.Println("My pointer memory location is: ", refPointer)
+	fmt.Println("My ref pointer value is: ", *refPointer)
+	fmt.Println("Another assigned var value is: ", anotherPtr)
 
+	*refPointer = *refPointer + 5
+
+	fmt.Println("My pointer value is: ", myPointer)
+	fmt.Println("My ref pointer value is: ", *refPointer)
+	fmt.Println("Another assigned var value is: ", anotherPtr)
 }
